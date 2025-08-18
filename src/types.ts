7c8779f14ed9e2332428c0c11f25f6019b762f22
mingli202 <vincentmingli@gmail.com>
2025-08-18 18:43:25 -0400

    types

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+import { Doc } from "../convex/_generated/dataModel";
+
+export type User = Doc<"users">;
+export type Profile = Doc<"profiles">;
+export type Setting = Doc<"settings">;
+export type Post = Doc<"posts">;
+export type Comment = Doc<"comments">;
+export type Reply = Doc<"replies">;
+export type Notification = Doc<"notifications">;
+export type Event = Doc<"events">;
+export type Club = Doc<"clubs">;
+export type Chat = Doc<"chats">;
+export type Message = Doc<"messages">;
