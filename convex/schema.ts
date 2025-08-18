ba81c96d3a7308a25975be36bead96460e4d21b5
mingli202 <vincentmingli@gmail.com>
2025-08-18 17:14:29 -0400

    feat:notifications schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -10,21 +10,23 @@ export default defineSchema({
 
   profiles: defineTable({
     userId: v.id("users"),
-    displayName: v.string(),
-    username: v.string(),
+
     birthday: v.string(),
+    displayName: v.string(),
+    major: v.string(),
+    school: v.string(),
+    username: v.string(),
     bio: v.optional(v.string()),
-    imageUrl: v.optional(v.string()),
     city: v.optional(v.string()),
+    imageUrl: v.optional(v.string()),
     following: v.array(v.id("users")),
     followers: v.array(v.id("users")),
-    school: v.string(),
     academicYear: v.number(),
-    major: v.string(),
   }).index("by_userId", ["userId"]),
 
   settings: defineTable({
     userId: v.id("users"),
+
     phoneNumber: v.optional(v.string()),
     gradeDisplayFormat: v.union(v.literal("Percentage"), v.literal("GPA")),
     private: v.boolean(),
@@ -32,23 +34,48 @@ export default defineSchema({
 
   posts: defineTable({
     authorId: v.id("users"),
-    description: v.string(),
-    likes: v.array(v.id("users")),
     comments: v.id("comments"),
+    likes: v.array(v.id("users")),
+
+    description: v.string(),
     imageUrl: v.string(),
   }).index("by_authorId", ["authorId"]),
 
   comments: defineTable({
-    postId: v.union(v.id("posts"), v.null()),
     authorId: v.id("users"),
-    content: v.string(),
     replies: v.array(v.id("replies")),
+    postId: v.union(v.id("posts"), v.null()),
+
+    content: v.string(),
   }).index("by_postId", ["postId"]),
 
   replies: defineTable({
+    authorId: v.id("users"),
     commentId: v.id("comments"),
     postId: v.id("posts"),
-    authorId: v.id("users"),
+
     text: v.string(),
   }).index("by_commentId", ["commentId"]),
+
+  notifications: defineTable({
+    userId: v.id("users"),
+    content: v.union(
+      v.object({
+        type: v.literal("follow"),
+        followerId: v.id("users"),
+      }),
+      v.object({
+        type: v.literal("like"),
+        postId: v.id("posts"),
+      }),
+      v.object({
+        type: v.literal("comment"),
+        commentId: v.id("comments"),
+      }),
+      v.object({
+        type: v.literal("reply"),
+        replyId: v.id("replies"),
+      }),
+    ),
+  }).index("by_userId", ["userId"]),
 });
