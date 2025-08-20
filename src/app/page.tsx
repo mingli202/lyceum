b0ec8690937474ad3da284e64eb745017b1e422f
mingli202 <vincentmingli@gmail.com>
2025-08-20 12:38:38 -0400

    added loading to initial load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,29 @@ import { useLayoutEffect, useState } from "react";
 import Login from "./login";
 import { login } from "@/actions/auth";
 import { useRouter } from "next/navigation";
+import LoadingSpinner from "@/ui/loadingSpinner";
 
 export default function Page() {
   const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
 
   useLayoutEffect(() => {
-    login().then((res) => {
+    (async () => {
+      const res = await login();
       if (res.ok) {
         router.push("/dashboard");
       } else {
         setIsLoading(false);
       }
-    });
+    })();
   }, []);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex h-full w-full items-center justify-center">
+        <LoadingSpinner />
+      </div>
+    );
   }
 
   return <Login />;
