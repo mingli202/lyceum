7dbe12a1c32db575b3652d0d78e593981e246916
mingli202 <vincentmingli@gmail.com>
2025-08-19 21:56:34 -0400

    feat: encryption service

---
+++ convex/EncryptionService.ts
@@ -0,0 +1,54 @@
+export class EncryptionService {
+  #publicKey: CryptoKey;
+  #privateKey: CryptoKey;
+
+  constructor(publicKey: CryptoKey, privateKey: CryptoKey) {
+    this.#publicKey = publicKey;
+    this.#privateKey = privateKey;
+  }
+
+  static async new(publicKeyString: string, privateKeyString: string) {
+    const publicKey = await crypto.subtle.importKey(
+      "spki",
+      Uint8Array.from(atob(publicKeyString), (t) => t.charCodeAt(0)),
+      { name: "RSA-PSS", hash: "SHA-256" },
+      false,
+      ["encrypt"],
+    );
+
+    const privateKey = await crypto.subtle.importKey(
+      "pkcs8",
+      Uint8Array.from(atob(privateKeyString), (t) => t.charCodeAt(0)),
+      { name: "RSA-PSS", hash: "SHA-256" },
+      false,
+      ["decrypt"],
+    );
+
+    return new EncryptionService(publicKey, privateKey);
+  }
+
+  async decrypt(value: string): Promise<string | null> {
+    const decrypted = await crypto.subtle
+      .decrypt(
+        { name: "RSA-PSS" },
+        this.#privateKey,
+        Uint8Array.from(atob(value), (t) => t.charCodeAt(0)),
+      )
+      .then((res) => new TextDecoder().decode(res))
+      .catch(() => null);
+
+    return decrypted;
+  }
+
+  async encrypt(value: string): Promise<string> {
+    const encrypted = await crypto.subtle.encrypt(
+      { name: "RSA-PSS" },
+      this.#publicKey,
+      new TextEncoder().encode(value),
+    );
+
+    const base64 = btoa(String.fromCharCode(...new Uint8Array(encrypted)));
+
+    return base64;
+  }
+}
