4ad4219c15798f3dc632bd0aa26d963013b0437e
mingli202 <vincentmingli@gmail.com>
2025-08-20 12:32:26 -0400

    ??

--- src/ui/loading.tsx
+++ src/ui/loading.tsx
@@ -4,6 +4,7 @@ export default function Loading() {
   return (
     <div className="flex flex-col items-center justify-center gap-2">
       <LoaderCircle className="h-10 w-10 animate-spin" />
+      <p>Loading</p>
     </div>
   );
 }
