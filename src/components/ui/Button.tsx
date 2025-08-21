d360a407be6f1e3d04880bd1c843dca356e9785f
mingli202 <vincentmingli@gmail.com>
2025-08-21 19:59:22 -0400

    custom button

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/utils/cn";
+import { HTMLAttributes } from "react";
+
+type ButtonProps = HTMLAttributes<HTMLButtonElement> & {
+  variant?: "special";
+};
+
+export default function Button({
+  className,
+  children,
+  variant,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={cn(
+        "text rounded p-2 transition-all duration-200 hover:cursor-pointer",
+        variant === "special" &&
+          "text-background bg-gradient-to-r from-blue-500 to-indigo-500 hover:from-blue-600 hover:to-indigo-600",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
