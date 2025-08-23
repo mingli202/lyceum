48557b13f9c26128fe0de8a2b52831b6ce0712b9
mingli202 <vincentmingli@gmail.com>
2025-08-23 10:47:23 -0400

    made sure to handle the unauthenticated case (rare)

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Authenticated } from "convex/react";
+import { Authenticated, Unauthenticated } from "convex/react";
 import Navbar from "./Navbar";
+import { LoadingSpinner } from "@/components/ui";
 
 export default function UserLayout({
   children,
@@ -9,11 +10,16 @@ export default function UserLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <Authenticated>
-      <div className="flex h-full w-full bg-gradient-to-br from-slate-50 to-blue-50">
-        <Navbar />
-        {children}
-      </div>
-    </Authenticated>
+    <>
+      <Authenticated>
+        <div className="flex h-full w-full bg-gradient-to-br from-slate-50 to-blue-50">
+          <Navbar />
+          {children}
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <LoadingSpinner />
+      </Unauthenticated>
+    </>
   );
 }
