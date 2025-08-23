33ec5d3fd04ebc1e74b4e564f9748eea6a3a304e
mingli202 <vincentmingli@gmail.com>
2025-08-23 12:09:00 -0400

    convex types

--- convex/types.ts
+++ convex/types.ts
@@ -28,3 +28,51 @@ export const DashboardData = v.object({
   average: v.optional(v.number()),
 });
 export type DashboardData = typeof DashboardData.type;
+
+export const CreateNewUserArgs = v.object({
+  signature: v.optional(v.string()),
+  school: v.string(),
+  major: v.string(),
+  firstName: v.string(),
+  lastName: v.optional(v.string()),
+  username: v.string(),
+  academicYear: v.number(),
+  city: v.optional(v.string()),
+  email: v.string(),
+  pictureUrl: v.optional(v.string()),
+  bio: v.optional(v.string()),
+  clerkId: v.string(),
+});
+export type CreateNewUserArgs = typeof CreateNewUserArgs.type;
+
+export const AddClassArgs = v.object({
+  signature: v.optional(v.string()),
+  code: v.string(),
+  title: v.string(),
+  professor: v.string(),
+  semester: v.union(
+    v.literal("Summer"),
+    v.literal("Fall"),
+    v.literal("Winter"),
+  ),
+  year: v.number(),
+  credits: v.number(),
+  classTimes: v.array(
+    v.object({
+      day: v.union(
+        v.literal("Monday"),
+        v.literal("Tuesday"),
+        v.literal("Wednesday"),
+        v.literal("Thursday"),
+        v.literal("Friday"),
+        v.literal("Saturday"),
+        v.literal("Sunday"),
+      ),
+      start: v.string(),
+      end: v.string(),
+    }),
+  ),
+  targetGrade: v.number(),
+  tasks: v.array(v.id("userTasks")),
+});
+export type AddClassArgs = typeof AddClassArgs.type;
