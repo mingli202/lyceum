2e8df65278338f66973d3e73988c46079dbd0deb
mingli202 <vincentmingli@gmail.com>
2025-08-22 22:04:53 -0400

    make sure children are in authenticated (for better dev exp)

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,14 +1,19 @@
+"use client";
+
+import { Authenticated } from "convex/react";
 import Navbar from "./Navbar";
 
-export default async function UserLayout({
+export default function UserLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-full w-full bg-gradient-to-br from-slate-50 to-blue-50">
-      <Navbar />
-      {children}
-    </div>
+    <Authenticated>
+      <div className="flex h-full w-full bg-gradient-to-br from-slate-50 to-blue-50">
+        <Navbar />
+        {children}
+      </div>
+    </Authenticated>
   );
 }
