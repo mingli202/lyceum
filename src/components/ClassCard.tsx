342947ce8f14749c8e6d2c3b96b4290152111d75
mingli202 <vincentmingli@gmail.com>
2025-08-26 17:26:56 -0400

    updated class card with Class util

--- src/components/ClassCard.tsx
+++ src/components/ClassCard.tsx
@@ -1,5 +1,6 @@
 import { DashboardData } from "@convex/types";
 import Link from "next/link";
+import { Card, CardFooter, CardHeader, CardTitle } from "./ui/Card";
 
 export function ClassCard({
   classInfo,
@@ -7,20 +8,22 @@ export function ClassCard({
   classInfo: DashboardData["classesInfo"][0];
 }) {
   return (
-    <Link
-      href={`/class?id=${classInfo.classId}`}
-      className="bg-background ring-foreground/10 z-0 flex flex-col rounded-lg p-3 text-sm shadow-md ring-1 transition hover:z-10 hover:cursor-pointer hover:shadow-lg"
-    >
-      <p className="w-fit max-w-full truncate rounded-full bg-indigo-100 px-1 py-0.5 text-xs text-indigo-800 ring-1 ring-indigo-300">
-        {classInfo.code}
-      </p>
-      <p className="mt-2 font-bold">{classInfo.title}</p>
-      <p>{classInfo.professor}</p>
-      <hr className="border-foreground/20 my-2" />
-      <div className="flex justify-between">
-        <p>Grade</p>
-        <p>{classInfo.grade.toFixed(2)}%</p>
-      </div>
+    <Link href={`/class?id=${classInfo.classId}`}>
+      <Card clickable>
+        <CardHeader>
+          <p className="w-fit max-w-full truncate rounded-full bg-indigo-100 px-1 py-0.5 text-xs text-indigo-800 ring-1 ring-indigo-300">
+            {classInfo.code}
+          </p>
+        </CardHeader>
+        <div>
+          <CardTitle>{classInfo.title}</CardTitle>
+          <p>{classInfo.professor}</p>
+        </div>
+        <CardFooter className="flex justify-between">
+          <p>Grade</p>
+          <p>{classInfo.grade.toFixed(2)}%</p>
+        </CardFooter>
+      </Card>
     </Link>
   );
 }
