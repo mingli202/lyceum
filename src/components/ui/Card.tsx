dec23d28ebfd32c13558a1bd5d599de907cbcf93
mingli202 <vincentmingli@gmail.com>
2025-08-26 17:26:46 -0400

    card ui component

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -3,19 +3,14 @@ import { HTMLAttributes, ReactNode } from "react";
 
 type CardProps = {
   children: ReactNode;
-  isClickable?: boolean;
+  clickable?: boolean;
 } & HTMLAttributes<HTMLDivElement>;
-export function Card({
-  children,
-  isClickable,
-  className,
-  ...props
-}: CardProps) {
+export function Card({ children, clickable, className, ...props }: CardProps) {
   return (
     <div
       className={cn(
-        "bg-background ring-foreground/10 z-0 flex items-center gap-2 rounded-lg p-3 text-sm shadow-md ring-1",
-        isClickable &&
+        "bg-background ring-foreground/10 z-0 gap-2 space-y-2 rounded-lg p-3 text-sm shadow-md ring-1",
+        clickable &&
           "transition hover:z-10 hover:cursor-pointer hover:shadow-lg",
         className,
       )}
@@ -26,16 +21,41 @@ export function Card({
   );
 }
 
-type CardHeaderProps = {
-  children: ReactNode;
-};
-export function CardHeader({ children }: CardHeaderProps) {
+export function CardHeader({
+  children,
+  className,
+  ...props
+}: { children: ReactNode } & HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className="w-fit rounded-full bg-indigo-100 px-1 py-0.5 text-xs text-indigo-800 ring-1 ring-indigo-300">
+    <div className={cn("text-muted-foreground text-sm", className)} {...props}>
       {children}
     </div>
   );
 }
-export function CardTitle({ children }: { children: ReactNode }) {}
-export function CardContent({ children }: { children: ReactNode }) {}
-export function CardFooter({ children }: { children: ReactNode }) {}
+
+export function CardTitle({
+  children,
+  className,
+  ...props
+}: { children: ReactNode } & HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div className={cn("font-bold", className)} {...props}>
+      {children}
+    </div>
+  );
+}
+
+export function CardFooter({
+  children,
+  className,
+  ...props
+}: { children: ReactNode } & HTMLAttributes<HTMLDivElement>) {
+  return (
+    <>
+      <hr className="border-foreground/20 my-2" />
+      <div className={cn("flex", className)} {...props}>
+        {children}
+      </div>
+    </>
+  );
+}
