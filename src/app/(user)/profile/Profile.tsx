8c005a89bd0d7c8a0ff92c012f3879755c54b141
mingli202 <vincentmingli@gmail.com>
2025-08-26 13:43:14 -0400

    tmp

--- src/app/(user)/profile/Profile.tsx
+++ src/app/(user)/profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components";
+import { Button, ButtonVariant } from "@/components";
 import { ProfileData } from "@convex/types";
 import { GraduationCap, MapPin, School } from "lucide-react";
 import UserActivity from "./UserActivity";
@@ -11,51 +11,55 @@ type ProfileProps = {
 export default function Profile({ data, currentClerkId }: ProfileProps) {
   return (
     <div className="flex h-full w-full justify-center overflow-x-hidden overflow-y-auto">
-      <section className="flex h-fit w-full max-w-xl flex-col gap-2 p-6">
-        <div className="bg-background ring-foreground/10 flex flex-col gap-4 rounded-lg p-4 shadow-md ring-1">
-          <div className="flex items-center gap-4">
-            <ProfilePicture
-              src={data.pictureUrl}
-              displayName={data.firstName}
-              className="h-16 w-16"
-            />
-            <div className="shrink-0">
-              <p className="overflow-ellipsis whitespace-nowrap">
-                {data.firstName} {data.lastName}
-              </p>
-              <p className="text-muted-foreground">@{data.username}</p>
-              <div className="text-muted-foreground flex gap-2 text-sm">
+      <section className="flex h-fit w-3xl flex-col gap-2 p-6">
+        <div className="bg-background ring-foreground/10 flex justify-center gap-10 rounded-lg p-4 shadow-md ring-1">
+          <ProfilePicture
+            src={data.pictureUrl}
+            displayName={data.firstName}
+            className="h-30 w-30"
+          />
+
+          <div className="flex flex-col gap-4">
+            <div className="shrink-0 space-y-1">
+              <div className="flex items-center gap-3">
+                <p>@{data.username}</p>
+                {currentClerkId === data.clerkId ? (
+                  <Button variant={ButtonVariant.Muted}>Edit</Button>
+                ) : (
+                  <Button variant={ButtonVariant.Special} className="py-1">
+                    Follow
+                  </Button>
+                )}
+              </div>
+              <div className="text-muted-foreground flex gap-3 text-sm">
                 <p>{data.followers.length} Followers</p>
                 <p>{data.following.length} Following</p>
               </div>
             </div>
-            <div className="basis-full" />
-            {currentClerkId === data.clerkId ? (
-              <Button>Edit</Button>
-            ) : (
-              <Button variant="special">Follow</Button>
-            )}
-          </div>
-          <div className="space-y-1">
-            {data.city && (
+
+            <div className="space-y-1">
+              {data.firstName} {data.lastName}
+              {data.city && (
+                <div className="flex gap-2">
+                  <MapPin />
+                  <span>{data.city}</span>
+                </div>
+              )}
               <div className="flex gap-2">
-                <MapPin />
-                <span>{data.city}</span>
+                <School />
+                <span>
+                  {data.school} {data.academicYear}
+                </span>
               </div>
-            )}
-            <div className="flex gap-2">
-              <School />
-              <span>
-                {data.school} {data.academicYear}
-              </span>
+              <div className="flex gap-2">
+                <GraduationCap />
+                <span>{data.major}</span>
+              </div>
             </div>
-            <div className="flex gap-2">
-              <GraduationCap />
-              <span>{data.major}</span>
-            </div>
+            {data.bio && <div>{data.bio}</div>}
           </div>
-          {data.bio && <div>{data.bio}</div>}
         </div>
+        <div className="space-y-1"></div>
         <UserActivity />
       </section>
     </div>
