179fae0e32694986378a4ecb555320116571fca0
mingli202 <vincentmingli@gmail.com>
2025-08-26 15:35:37 -0400

    some better profile change

--- src/app/(user)/profile/Profile.tsx
+++ src/app/(user)/profile/Profile.tsx
@@ -12,7 +12,7 @@ export default function Profile({ data, currentClerkId }: ProfileProps) {
   return (
     <div className="flex h-full w-full justify-center overflow-x-hidden overflow-y-auto">
       <section className="flex h-fit w-full max-w-3xl flex-col gap-2 p-6">
-        <div className="flex justify-between gap-4 rounded-lg p-6">
+        <div className="bg-background flex justify-between gap-4 rounded-lg p-6 shadow-sm">
           <div className="space-y-4">
             <div className="flex items-center gap-4">
               <ProfilePicture
@@ -21,13 +21,17 @@ export default function Profile({ data, currentClerkId }: ProfileProps) {
                 className="h-20 w-20"
               />
               <div className="shrink-0 space-y-1">
-                <p className="overflow-ellipsis whitespace-nowrap">
+                <p className="font-bold overflow-ellipsis whitespace-nowrap">
                   {data.firstName} {data.lastName}
                 </p>
                 <p className="text-muted-foreground">@{data.username}</p>
-                <div className="text-muted-foreground flex gap-2">
-                  <p>{data.followers.length} Followers</p>
-                  <p>{data.following.length} Following</p>
+                <div className="t flex gap-2">
+                  <p>
+                    {data.followers.length} <span className="">Followers</span>
+                  </p>
+                  <p>
+                    {data.following.length} <span className="">Followings</span>
+                  </p>
                 </div>
               </div>
               <div className="basis-full" />
@@ -36,23 +40,22 @@ export default function Profile({ data, currentClerkId }: ProfileProps) {
           </div>
 
           <div className="flex flex-col items-end space-y-1">
+            <div className="flex gap-2">
+              <span>{data.school}</span>
+              <School />
+            </div>
+            <div className="flex gap-2">
+              <span>
+                {data.academicYear - 2024}st Year · {data.major}
+              </span>
+              <GraduationCap />
+            </div>
             {data.city && (
               <div className="flex gap-2">
                 <span>{data.city}</span>
                 <MapPin />
               </div>
             )}
-            <div className="flex gap-2">
-              <span>
-                {data.school} {data.academicYear}
-              </span>
-              <School />
-            </div>
-            <div className="flex gap-2">
-              <span>{data.major}</span>
-              <GraduationCap />
-            </div>
-            {data.bio && <div>{data.bio}</div>}
           </div>
         </div>
         <div className="space-y-1"></div>
