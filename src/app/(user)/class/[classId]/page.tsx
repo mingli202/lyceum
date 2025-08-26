9cd5dc9b38f95fba7bb42e682df60e9eabfb15fa
mingli202 <vincentmingli@gmail.com>
2025-08-26 06:56:57 -0400

    feat: client side url params

--- src/app/(user)/class/[classId]/page.tsx
+++ src/app/(user)/class/[classId]/page.tsx
@@ -1,10 +1,30 @@
-import Class from "./Class";
+"use client";
 
-export default async function ClassPage({
-  params,
-}: {
-  params: Promise<{ classId: string }>;
-}) {
-  const { classId } = await params;
-  return <Class classId={classId} />;
+import { useParams } from "next/navigation";
+import Class from "./Class";
+import { useQuery } from "convex/react";
+import { api } from "@convex/_generated/api";
+import { LoadingSpinner } from "@/components";
+
+export default function ClassPage() {
+  const params = useParams();
+
+  const classId = params.classId?.toString();
+  const classData = classId
+    ? useQuery(api.queries.getClassPageData, { classId })
+    : "Class not found";
+
+  if (!classData) {
+    return <LoadingSpinner />;
+  }
+
+  if (typeof classData === "string") {
+    return (
+      <div className="flex h-full w-full items-center justify-center text-red-500">
+        {classData}
+      </div>
+    );
+  }
+
+  return <Class classData={classData} />;
 }
