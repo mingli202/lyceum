f3f50866ffa02aab84b615a0e2beb36a4a52e191
mingli202 <vincentmingli@gmail.com>
2025-08-26 18:02:45 -0400

    assignment type

--- convex/schema.ts
+++ convex/schema.ts
@@ -71,6 +71,13 @@ export default defineSchema({
     scoreObtained: v.number(),
     scoreTotal: v.number(),
     weight: v.number(),
+    type: v.optional(
+      v.union(
+        v.literal("Exam"),
+        v.literal("Assignments"),
+        v.literal("Project"),
+      ),
+    ),
   })
     .index("by_userId", ["userId"]) // for getting all the user's classes
     .index("by_classId", ["classId"]) // for getting all tasks of a class
