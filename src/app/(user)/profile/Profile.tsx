4a3adbae86c9e655a475b334e56831e52e99bb94
mingli202 <vincentmingli@gmail.com>
2025-08-26 15:15:48 -0400

    another layout

--- src/app/(user)/profile/Profile.tsx
+++ src/app/(user)/profile/Profile.tsx
@@ -11,50 +11,46 @@ type ProfileProps = {
 export default function Profile({ data, currentClerkId }: ProfileProps) {
   return (
     <div className="flex h-full w-full justify-center overflow-x-hidden overflow-y-auto">
-      <section className="flex h-fit w-3xl flex-col gap-2 p-6">
-        <div className="bg-background ring-foreground/10 flex justify-center gap-10 rounded-lg p-4 shadow-md ring-1">
-          <ProfilePicture
-            src={data.pictureUrl}
-            displayName={data.firstName}
-            className="h-30 w-30"
-          />
-
-          <div className="flex flex-col gap-4">
-            <div className="shrink-0 space-y-1">
-              <div className="flex items-center gap-3">
-                <p>@{data.username}</p>
-                {currentClerkId === data.clerkId ? (
-                  <Button variant={ButtonVariant.Muted}>Edit</Button>
-                ) : (
-                  <Button variant={ButtonVariant.Special} className="py-1">
-                    Follow
-                  </Button>
-                )}
-              </div>
-              <div className="text-muted-foreground flex gap-3 text-sm">
-                <p>{data.followers.length} Followers</p>
-                <p>{data.following.length} Following</p>
-              </div>
-            </div>
-
-            <div className="space-y-1">
-              {data.firstName} {data.lastName}
-              {data.city && (
-                <div className="flex gap-2">
-                  <MapPin />
-                  <span>{data.city}</span>
+      <section className="flex h-fit w-full max-w-3xl flex-col gap-2 p-6">
+        <div className="flex justify-between gap-4 rounded-lg p-6">
+          <div className="space-y-4">
+            <div className="flex items-center gap-4">
+              <ProfilePicture
+                src={data.pictureUrl}
+                displayName={data.firstName}
+                className="h-20 w-20"
+              />
+              <div className="shrink-0 space-y-1">
+                <p className="overflow-ellipsis whitespace-nowrap">
+                  {data.firstName} {data.lastName}
+                </p>
+                <p className="text-muted-foreground">@{data.username}</p>
+                <div className="text-muted-foreground flex gap-2">
+                  <p>{data.followers.length} Followers</p>
+                  <p>{data.following.length} Following</p>
                 </div>
-              )}
-              <div className="flex gap-2">
-                <School />
-                <span>
-                  {data.school} {data.academicYear}
-                </span>
               </div>
+              <div className="basis-full" />
+            </div>
+            {data.bio && <div>{data.bio}</div>}
+          </div>
+
+          <div className="flex flex-col items-end space-y-1">
+            {data.city && (
               <div className="flex gap-2">
-                <GraduationCap />
-                <span>{data.major}</span>
+                <span>{data.city}</span>
+                <MapPin />
               </div>
+            )}
+            <div className="flex gap-2">
+              <span>
+                {data.school} {data.academicYear}
+              </span>
+              <School />
+            </div>
+            <div className="flex gap-2">
+              <span>{data.major}</span>
+              <GraduationCap />
             </div>
             {data.bio && <div>{data.bio}</div>}
           </div>
