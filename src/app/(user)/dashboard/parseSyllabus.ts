bb20c75868718ec451ea4c54a9446231c21a9c63
mingli202 <vincentmingli@gmail.com>
2025-08-25 22:33:40 -0400

    refac: using actual mutations

--- src/app/(user)/dashboard/parseSyllabus.ts
+++ src/app/(user)/dashboard/parseSyllabus.ts
@@ -1,10 +1,6 @@
 "use server";
 
-import { fetchMutation } from "convex/nextjs";
-import { SignatureService } from "@convex/services/signatureService";
 import { AddClassArgs } from "@convex/types";
-import { api } from "@convex/_generated/api";
-import { auth } from "@clerk/nextjs/server";
 import OpenAI from "openai";
 import { ParsedFileResponse } from "@/types";
 
@@ -12,105 +8,7 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const signatureService = new SignatureService();
-
-type ClassTime = AddClassArgs["classTimes"][0];
-
-function checkForOverlap(classTimes: ClassTime[]) {
-  const days: { [k in ClassTime["day"]]?: [string, string][] } = {};
-  for (const classTime of classTimes) {
-    if (!days[classTime.day]) {
-      days[classTime.day] = [];
-    }
-    days[classTime.day]!.push([classTime.start, classTime.end]);
-  }
-
-  for (const times of Object.values(days)) {
-    // loop through each pair of times
-    // and compare them with the other pairs
-    for (let i = 0; i < times.length - 1; i++) {
-      for (let k = i + 1; k < times.length; k++) {
-        const [start1, end1] = times[i];
-        const [start2, end2] = times[k];
-
-        // formats are like "09:00" and "10:00"
-        const nStart1 = parseInt(start1.replace(":", ""));
-        const nStart2 = parseInt(start2.replace(":", ""));
-        const nEnd1 = parseInt(end1.replace(":", ""));
-        const nEnd2 = parseInt(end2.replace(":", ""));
-
-        if (nStart1 >= nEnd1 || nStart2 >= nEnd2) {
-          throw new Error("Class ends in the past???");
-        }
-
-        if (
-          nStart1 === nStart2 ||
-          nEnd1 === nEnd2 ||
-          (nStart1 > nStart2 && nEnd2 > nStart1) ||
-          (nStart2 > nStart1 && nEnd1 > nStart2)
-        ) {
-          return true;
-        }
-      }
-    }
-  }
-
-  return false;
-}
-
-export async function addClass(
-  formData: FormData,
-  classTimes: AddClassArgs["classTimes"],
-): Promise<string | null> {
-  const { userId } = await auth();
-
-  const title = formData.get("class-title")!.toString();
-  const code = formData.get("class-code")!.toString();
-  const professor = formData.get("class-prof")!.toString();
-  const semester = formData.get("class-semester")!.toString();
-  const year = formData.get("class-year")!.toString();
-  const credits = formData.get("class-credits")!.toString();
-  const targetGrade = formData.get("class-target-grade")!.toString();
-
-  if (!["Summer", "Fall", "Winter"].includes(semester)) {
-    return "Invalid semester";
-  }
-
-  try {
-    if (checkForOverlap(classTimes)) {
-      return "Class times overlap";
-    }
-  } catch (e) {
-    if (e instanceof Error) {
-      return e.message;
-    }
-    return "Error while checking for class times overlap";
-  }
-
-  const body: AddClassArgs = {
-    code,
-    title,
-    professor,
-    semester: semester as AddClassArgs["semester"],
-    year: parseInt(year),
-    credits: parseInt(credits),
-    classTimes,
-    targetGrade: parseInt(targetGrade),
-    tasks: [],
-  };
-
-  const signature = await signatureService.sign({
-    ...body,
-    clerkId: userId,
-  });
-
-  return await fetchMutation(api.mutations.addClass, {
-    ...body,
-    signature,
-  }).catch(() => "Couldn't add class");
-}
-
-export async function addClassFromSyllabus(file: File) {
+export default async function parseSyllabus(file: File): Promise<AddClassArgs> {
   const res = await openai.files.create({
     file,
     purpose: "user_data",
@@ -260,21 +158,8 @@ export async function addClassFromSyllabus(file: File) {
   const data = ParsedFileResponse.parse(response.output_parsed);
 
   const classTimes = data.classTimes;
-
-  try {
-    if (checkForOverlap(classTimes)) {
-      return "Class times overlap";
-    }
-  } catch (e) {
-    if (e instanceof Error) {
-      return e.message;
-    }
-    return "Error while checking for class times overlap";
-  }
-
   const tasks = data.tasks;
 
-  const { userId } = await auth();
   const body: AddClassArgs = {
     code: data.code,
     title: data.title,
@@ -286,20 +171,5 @@ export async function addClassFromSyllabus(file: File) {
     targetGrade: 85,
     tasks,
   };
-
-  const signature = await signatureService
-    .sign({
-      ...body,
-      clerkId: userId,
-    })
-    .catch(() => null);
-
-  if (!signature) {
-    return "Something went wrong, please try again";
-  }
-
-  return await fetchMutation(api.mutations.addClass, {
-    ...body,
-    signature,
-  }).catch(() => "Couldn't add class");
+  return body;
 }
