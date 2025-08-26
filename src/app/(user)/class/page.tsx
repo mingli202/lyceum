e37e6a6fde93ef12982bc2475145b7c0a93f35f2
mingli202 <vincentmingli@gmail.com>
2025-08-26 08:55:21 -0400

    moved to search params instead

--- src/app/(user)/class/page.tsx
+++ src/app/(user)/class/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { useParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import Class from "./Class";
 import { useQuery } from "convex/react";
 import { api } from "@convex/_generated/api";
 import { LoadingSpinner } from "@/components";
 
 export default function ClassPage() {
-  const params = useParams();
+  const params = useSearchParams();
 
-  const classId = params.classId?.toString();
+  const classId = params.get("id")?.toString();
   const classData = useQuery(api.queries.getClassPageData, { classId });
 
   if (!classData) {
