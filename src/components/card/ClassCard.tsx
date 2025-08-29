c3dadb9c7b0eb40105786b0b5c2c230e200f2df3
mingli202 <vincentmingli@gmail.com>
2025-08-28 21:38:37 -0400

    class card expand

--- src/components/card/ClassCard.tsx
+++ src/components/card/ClassCard.tsx
@@ -8,18 +8,19 @@ export function ClassCard({
   classInfo: DashboardData["classesInfo"][0];
 }) {
   return (
-    <Link href={`/class?id=${classInfo.classId}`}>
-      <Card clickable>
-        <CardHeader>
+    <Link href={`/class?id=${classInfo.classId}`} className="h-full">
+      <Card clickable className="h-full">
+        <CardHeader className="shrink-0">
           <p className="w-fit max-w-full truncate rounded-full bg-indigo-100 px-1 py-0.5 text-xs text-indigo-800 ring-1 ring-indigo-300">
             {classInfo.code}
           </p>
         </CardHeader>
-        <div>
+        <div className="shrink-0">
           <CardTitle>{classInfo.title}</CardTitle>
           <p>{classInfo.professor}</p>
         </div>
-        <CardFooter className="flex justify-between">
+        <div className="basis-full" />
+        <CardFooter className="flex shrink-0 justify-between">
           <p>Grade</p>
           <p>{classInfo.grade.toFixed(1)}%</p>
         </CardFooter>
